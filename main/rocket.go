98391b3c820a9b8ee154dc9652ab8210c6f79b8f
Timotije <timotije.tuf@gmail.com>
2024-07-01 13:16:46 +0200

    Basics

---
+++ main/rocket.go
@@ -0,0 +1,23 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"rocket\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/sprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 12.0
+    y: 7.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.06082621
+    z: 0.0
+    w: 0.9981484
+  }
+}
